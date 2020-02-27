808294c19cc40145654ca13724bf70f3c6847d0d
Jordan Melendez <jordan.melendez@datarobot.com>
2020-02-26 20:26:09 -0500

    Updated rcparams and added ellipses

--- nuclear_matter/graphs.py
+++ nuclear_matter/graphs.py
@@ -1,4 +1,7 @@
 import matplotlib as mpl
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
+import numpy as np
 
 
 def setup_rc_params():
@@ -16,7 +19,6 @@ def setup_rc_params():
     mpl.rcParams['axes.edgecolor'] = black
     # mpl.rcParams['axes.xmargin'] = 0
     mpl.rcParams['axes.labelcolor'] = black
-    mpl.rcParams['axes.linewidth']
     mpl.rcParams['axes.titlesize'] = fontsize
 
     mpl.rcParams['ytick.direction'] = 'in'
@@ -25,10 +27,26 @@ def setup_rc_params():
     mpl.rcParams['ytick.labelsize'] = fontsize
     mpl.rcParams['xtick.color'] = black
     mpl.rcParams['ytick.color'] = black
-    mpl.rcParams['xtick.minor.size'] = 2.4
+    # Make the ticks thin enough to not be visible at the limits of the plot (over the axes border)
+    mpl.rcParams['xtick.major.width'] = mpl.rcParams['axes.linewidth'] * 0.95
+    mpl.rcParams['ytick.major.width'] = mpl.rcParams['axes.linewidth'] * 0.95
+    # The minor ticks are little too small, make them both bigger.
+    mpl.rcParams['xtick.minor.size'] = 2.4  # Default 2.0
     mpl.rcParams['ytick.minor.size'] = 2.4
+    mpl.rcParams['xtick.major.size'] = 3.9  # Default 3.5
+    mpl.rcParams['ytick.major.size'] = 3.9
 
+    ppi = 72  # points per inch
+    # dpi = 150
     mpl.rcParams['figure.titlesize'] = fontsize
+    mpl.rcParams['figure.dpi'] = 150  # To show up reasonably in notebooks
+    mpl.rcParams['figure.constrained_layout.use'] = True
+    # 0.02 and 3 points are the defaults:
+    # can be changed on a plot-by-plot basis using fig.set_constrained_layout_pads()
+    mpl.rcParams['figure.constrained_layout.wspace'] = 0.0
+    mpl.rcParams['figure.constrained_layout.hspace'] = 0.0
+    mpl.rcParams['figure.constrained_layout.h_pad'] = 3. / ppi  # 3 points
+    mpl.rcParams['figure.constrained_layout.w_pad'] = 3. / ppi
 
     mpl.rcParams['legend.title_fontsize'] = fontsize
     mpl.rcParams['legend.fontsize'] = fontsize
@@ -41,8 +59,9 @@ def setup_rc_params():
 
     mpl.rcParams['hatch.linewidth'] = 0.5
 
-    mpl.rc('savefig', transparent=False, bbox='tight', pad_inches=0.05, dpi=350, format='png')
-    mpl.rcParams['figure.dpi'] = 150
+    # bbox = 'tight' can distort the figure size when saved (that's its purpose).
+    # mpl.rc('savefig', transparent=False, bbox='tight', pad_inches=0.04, dpi=350, format='png')
+    mpl.rc('savefig', transparent=False, bbox=None, dpi=400, format='pdf')
 
 
 def lighten_color(color, amount=0.5):
@@ -63,3 +82,90 @@ def lighten_color(color, amount=0.5):
         c = color
     c = colorsys.rgb_to_hls(*mc.to_rgb(c))
     return colorsys.hls_to_rgb(c[0], 1 - amount * (1 - c[1]), c[2])
+
+
+def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', show_scatter=False, **kwargs):
+    """
+    Create a plot of the covariance confidence ellipse of *x* and *y*.
+
+    Parameters
+    ----------
+    x, y : array-like, shape (n, )
+        Input data.
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+
+    Other parameters
+    ----------------
+    kwargs : `~matplotlib.patches.Patch` properties
+    """
+
+    if x.size != y.size:
+        raise ValueError("x and y must be the same size")
+
+    mean_x = np.mean(x)
+    mean_y = np.mean(y)
+    mean = np.array([mean_x, mean_y])
+    cov = np.cov(x, y)
+
+    if show_scatter:
+        from .stats_utils import darken_color
+        scat_color = darken_color(facecolor, 0.5)
+        ax.plot(x, y, ls='', marker='.', markersize=0.6, color=scat_color)
+
+    return confidence_ellipse_mean_cov(mean, cov, ax, n_std=n_std, facecolor=facecolor, **kwargs)
+
+
+def confidence_ellipse_mean_cov(mean, cov, ax, n_std=3.0, facecolor='none', **kwargs):
+    """
+    Create a plot of the covariance confidence ellipse of *x* and *y*.
+
+    Parameters
+    ----------
+    x, y : array-like, shape (n, )
+        Input data.
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+
+    Other parameters
+    ----------------
+    kwargs : `~matplotlib.patches.Patch` properties
+    """
+    pearson = cov[0, 1] / np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensionl dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0),
+        width=ell_radius_x * 2,
+        height=ell_radius_y * 2,
+        facecolor=facecolor,
+        **kwargs)
+
+    # Calculating the standard deviation of x from
+    # the square root of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    # calculating the stdandard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+
+    transf = transforms.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean[0], mean[1])
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
