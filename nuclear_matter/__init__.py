642a84e80c9815242f0b73c222ba7b37a218b3a6
Jordan Melendez <jordan.melendez@datarobot.com>
2020-02-26 20:28:17 -0500

    New funcs

--- nuclear_matter/__init__.py
+++ nuclear_matter/__init__.py
@@ -6,6 +6,19 @@ from .matter import fermi_momentum
 from .matter import ratio_kf
 from .matter import Lb_prior
 from .matter import Lb_logprior
+from .matter import compute_pressure
+from .matter import compute_pressure_cov
+from .matter import compute_slope
+from .matter import compute_slope_cov
+from .matter import compute_compressibility
+from .matter import compute_compressibility_cov
+from .matter import compute_speed_of_sound
+from .matter import kf_derivative_wrt_density
+from .matter import kf_2nd_derivative_wrt_density
 
 from .graphs import setup_rc_params
 from .graphs import lighten_color
+from .graphs import confidence_ellipse_mean_cov
+from .graphs import confidence_ellipse
+
+from .utils import InputData
